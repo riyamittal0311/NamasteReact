a5322480b7b42de575add11e16637b3ca023c989
riyamittal0311 <118875426+riyamittal0311@users.noreply.github.com>
2022-12-31 17:19:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement('h1',{id:'heading1'},'Heading 1')
+const heading2= React.createElement('h2',null,'Heading 2')
+const parent = React.createElement('div',{},[heading,heading2])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+console.log(heading,heading2)
+
+root.render(parent)
