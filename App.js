91bf8815edae1754420abf34d276be840e59ad5d
riyamittal0311 <riyamittal2003@gmail.com>
2023-01-01 21:02:50 +0530

    Project Assignment2

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-const heading = React.createElement('h1',{id:'heading1'},'Heading 1')
-const heading2= React.createElement('h2',null,'Heading 2')
-const parent = React.createElement('div',{},[heading,heading2])
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-console.log(heading,heading2)
+const heading = React.createElement("h1", { id: "heading1" }, "Heading 1");
+const heading2 = React.createElement("h2", null, "Heading 2");
+const parent = React.createElement("div", {}, [heading, heading2]);
 
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading, heading2);
+
+root.render(parent);
