838d95c84b4c433f69cceab2896e0568a060883b
riyamittal0311 <riyamittal2003@gmail.com>
2023-01-07 21:50:12 +0530

    chapter 04 project

--- App.js
+++ App.js
@@ -2,30 +2,70 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 import logo from "./images/logo.jpg";
+import { restaurantList } from "./data";
 
+const Title = () => {
+  return (
+    <div className="img">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={logo} />
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
       </div>
-      <div className="search">
-        <div>
-          <input type="text"  placeholder="Search..."/>
-          <div className="search_icon">
-          <i class="fa fa-search" aria-hidden="true"></i>
-          </div>
-        </div>
-      </div>
-      <div className="user_icon">
-        <i className="fa fa-regular fa-user"></i>
-      </div>
-    </header>
+    </div>
   );
 };
 
+const Restaurant = ({
+  name,
+  cloudinaryImageId,
+  cuisines,
+  lastMileTravelString,
+}) => {
+  return (
+    <div className="item">
+      <img
+        alt="RestaurantLogo"
+        src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/${cloudinaryImageId}`}
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(', ')}</h4>
+      <h5>{lastMileTravelString} minutes</h5>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="list">
+      {restaurantList.map((restaurant) => {
+        return <Restaurant {...restaurant.data} />;
+      })}
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
 };
 
 const root = ReactDom.createRoot(document.getElementById("root"));
